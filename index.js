d7f8d8f409c8d0a99c074b4a75c9a80802dc8bbf
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-11 17:27:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+function countDown(n) {
+  let i = n 
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
+
+// countDown(10)
+
+
+// writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise') )
+//         .to.deep.eq(['Thank you, Lisa, for the wonderful surprise gift!',
+
+// function writeCards(array, word) {
+//   var n = [];
+//   for (let count = 0; count < array.length; count++) {
+//     n.push(`Thank you, ${array[count]}, for the wonderful ${word} gift!`);
+    
+//   }
+//   console.log(n);
+// }
+
+function writeCards(array, word) {
+  var n = []
+  array.forEach(element => {
+    n.push(`Thank you, ${element}, for the wonderful ${word} gift!`);
+  })
+  return n
+}
